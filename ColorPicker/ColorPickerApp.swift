c1fb7fcc21d58865bdd24c7e4c00412337661376
follow <follow12587@gmail.com>
2024-03-06 10:12:21 +1000

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Сергей Дубовой on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
